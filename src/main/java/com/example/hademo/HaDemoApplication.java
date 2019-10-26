5673680383150bce958dfd4333a312eb180d7be6
Eddú Meléndez <eddu.melendez@gmail.com>
2019-10-26 10:40:30 -0500

    Initial commit

---
+++ src/main/java/com/example/hademo/HaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hademo/HaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
